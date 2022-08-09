c78f21ba808f9eeb878b944b311e5ec686031066
jaferIdrees <jaferll@gmail.com>
2022-08-09 17:25:22 +0300

    Build the todo app

---
+++ src/components/TodoContainer.js
@@ -0,0 +1,115 @@
+import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import {
+  Routes,
+  Route,
+} from 'react-router-dom';
+import TodosList from './TodosList';
+import Header from './Header';
+import InputTodo from './InputTodo';
+import About from '../pages/About';
+import NotMatch from '../pages/NotMatch';
+
+class TodoContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [],
+    };
+  }
+
+  componentDidMount() {
+    const temp = localStorage.getItem('todos');
+    const loadedTodos = JSON.parse(temp);
+    if (loadedTodos) {
+      this.setState({
+        todos: loadedTodos,
+      });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.todos !== { ...this.state }.todos) {
+      const temp = JSON.stringify({ ...this.state }.todos);
+      localStorage.setItem('todos', temp);
+    }
+  }
+
+  handleChange = (id) => {
+    this.setState((prevState) => ({
+      todos: prevState.todos.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            completed: !todo.completed,
+          };
+        }
+        return todo;
+      }),
+    }));
+  };
+
+  delTodo = (id) => {
+    this.setState({
+      todos: [
+        // eslint-disable-next-line react/no-access-state-in-setstate
+        { ...this.state }.todos.filter((todo) => todo.id !== id),
+      ],
+    });
+  };
+
+  addTodoItem = (title) => {
+    const newTodo = {
+      id: uuidv4(),
+      title,
+      completed: false,
+    };
+    this.setState({
+      // eslint-disable-next-line react/no-access-state-in-setstate
+      todos: [{ ...this.state }.todos, newTodo],
+    });
+  };
+
+  setUpdate = (updatedTitle, id) => {
+    this.setState({
+      // eslint-disable-next-line react/no-access-state-in-setstate
+      todos: { ...this.state }.todos.map((todo) => {
+        const retunTodo = todo;
+        if (todo.id === id) {
+          retunTodo.title = updatedTitle;
+        }
+        return retunTodo;
+      }),
+    });
+  };
+
+  Body = () => (
+    <>
+      <div className="container">
+        <div className="inner">
+          <Header />
+          <InputTodo addTodoProps={this.addTodoItem} />
+          <TodosList
+            todos={{ ...this.state }.todos}
+            handleChangeProps={this.handleChange}
+            deleteTodoProps={this.delTodo}
+            setUpdate={this.setUpdate}
+          />
+        </div>
+      </div>
+    </>
+  );
+
+  render() {
+    return (
+
+      <Routes>
+        <Route exact path="/" element={<this.Body />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotMatch />} />
+      </Routes>
+
+    );
+  }
+}
+export default TodoContainer;
